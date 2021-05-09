06dc867c5234bd0f3216481d31616560a9dca78a
lcy <1046157113@qq.com>
2021-05-09 11:33:24 +0800

    first commit

---
+++ hig_mv/hig_vector.c
@@ -0,0 +1,102 @@
+/*BHEADER**********************************************************************
+ * 
+ * XHYPRE 2020.10.29
+ *
+ * autor: Chuanying Li
+ *
+ ***********************************************************************EHEADER*/
+ 
+
+/******************************************************************************
+ *
+ * Member functions for hypre_Vector class.
+ *
+ *****************************************************************************/
+
+#include "hig_mv.h"
+#include "seq_mv.h"
+#include <assert.h>
+
+#ifndef SPLITTER
+#define SPLITTER 134217729.0               // = 2^27 + 1
+#endif
+
+typedef struct doubledouble{
+    double H;
+    double L;
+}dd_real;
+	
+/* Computes fl(a+b) and err(a+b).  */
+static dd_real two_sum(double a, double b) {
+    dd_real res;
+	double bb;
+    res.H = a + b;
+    bb = res.H - a;
+    res.L = (a - (res.H - bb)) + (b - bb);
+    return res;
+}
+	
+/* Computes high word and lo word of a */
+static dd_real split(double a) {
+    double temp;
+    dd_real res;
+    temp = SPLITTER * a;
+    res.H = temp - (temp - a);
+    res.L = a - res.H;
+    return res;
+}
+
+/* Computes fl(a*b) and err(a*b). */
+static dd_real two_prod(double a, double b) {
+    dd_real res,aa,bb;
+    res.H = a * b;
+    aa=split(a);
+    bb=split(b);
+    res.L = ((aa.H * bb.H - res.H) + aa.H * bb.L + aa.L * bb.H) + aa.L * bb.L;
+    return res;
+}
+
+
+/*--------------------------------------------------------------------------
+ * hypre_HIGSeqVectorInnerProd
+ *--------------------------------------------------------------------------*/
+
+HYPRE_Real   hypre_HIGSeqVectorInnerProd( hypre_Vector *x,
+                                       hypre_Vector *y )
+{
+#ifdef HYPRE_PROFILE
+   hypre_profile_times[HYPRE_TIMER_ID_BLAS1] -= hypre_MPI_Wtime();
+#endif
+
+   HYPRE_Complex *x_data = hypre_VectorData(x);
+   HYPRE_Complex *y_data = hypre_VectorData(y);
+   HYPRE_Int      size   = hypre_VectorSize(x);
+           
+   HYPRE_Int      i;
+
+   HYPRE_Real     result = 0.0;
+   
+   dd_real temp1;
+
+   size *=hypre_VectorNumVectors(x);
+   
+
+#ifdef HYPRE_USING_OPENMP
+#pragma omp parallel for private(i) reduction(+:result) HYPRE_SMP_SCHEDULE
+#endif
+   dd_real temps= {0.0,0.0};
+   for (i = 0; i < size; i++)
+   {
+	   temp1 = two_prod(hypre_conj(y_data[i]), x_data[i]);
+	   temps = two_sum(temps.H, temp1.H);
+	   result = result + (temp1.L + temps.L) ;
+   }
+   result = result + temps.H;
+
+#ifdef HYPRE_PROFILE
+   hypre_profile_times[HYPRE_TIMER_ID_BLAS1] += hypre_MPI_Wtime();
+#endif
+   return result;
+}
+/* XHYPRE 2020.11.29 */
+
